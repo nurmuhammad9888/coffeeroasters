e6dec2eecad9fadbcfeb7928ae4049a401909d0e
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2022-08-12 05:20:57 +0500

    Add new elemint

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 let elBtnOpen = document.querySelector(".open-btn")
 let elNavShow = document.querySelector(".site-header__nav-show")
 let elOverlayshow = document.querySelector(".overlay")
-
+let elBody = document.querySelector("body")
 
 elBtnOpen.addEventListener("click", function(){
     elNavShow.classList.toggle("site-header__nav-show-active")
     elBtnOpen.classList.toggle("open-btn-show")
     elOverlayshow.classList.toggle("overlay-show")
+    elBody.classList.toggle("body-active")
 })
